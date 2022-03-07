db58f35da27d3c401b1f3729c58191e676c1ade4
Júlio Cesar <jc561031@gmail.com>
2022-03-07 15:36:04 -0300

    Inicio do projeto

---
+++ backend/src/main/java/com/EstudosJczar/springreactdsmovie/SpringReactDsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.EstudosJczar.springreactdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactDsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactDsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/EstudosJczar/springreactdsmovie/SpringReactDsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EstudosJczar.springreactdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactDsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
